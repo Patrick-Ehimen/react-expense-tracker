871366da508c1a9c3daf2227fcd1cfe9b11dc1db
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-05-20 14:02:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    { title: "Car Insurance", amount: 294.67, date: new Date(2023, 4, 20) },
+    { title: "Car Insurance", amount: 294.67, date: new Date(2023, 4, 20) },
+    { title: "Car Insurance", amount: 294.67, date: new Date(2023, 4, 20) },
+  ];
   return (
     <div>
       <ExpenseItem></ExpenseItem>
