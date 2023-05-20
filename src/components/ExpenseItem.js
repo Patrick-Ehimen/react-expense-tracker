ae2bbef5c0c8528e32b218a0480b6ae777c782f5
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-05-20 14:33:19 +0100

    Update ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,10 @@
 import "./ExpenseItem.css";
+
 function ExpenseItem() {
+  const expenseDate = new Date(2023, 4, 28);
+  const expenseTitle = "Paypal Donations";
+  const expenseAmount = 294.67;
+
   return (
     <div className="expense-item">
       <div>{expenseDate.toISOString()}</div>
