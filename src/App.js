3be44da0f8424a32261014d4a33aefdd8bf5e4ee
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-05-21 17:27:02 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -29,6 +29,6 @@ function App() {
       <h2>Let's get started!</h2>
     </div>
   );
-}
+};
 
 export default App;
